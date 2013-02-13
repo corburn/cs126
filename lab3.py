d9b224abcf45fa526e34e3e090f51868c5f659df
Jason Travis <infomaniac7@gmail.com>
2013-02-12 23:28:11 -0700

    add: Puzzle 3 Spinning out of Control

--- lab3.py
+++ lab3.py
@@ -3,34 +3,38 @@ import math
 
 HEIGHT = 64 # Really 96, but the lower third overlaps
 WIDTH = 64
-SQUARE = 8
 
-stage = Stage(8*WIDTH, 8*HEIGHT + 32)
+stage = Stage(9*WIDTH, 8*HEIGHT + 32)
 stage.color = 'white'
 
-def add_block(name, x, y, a):
+def add_block(name, x, y, a, r):
 	global stage
 	block = Bitmap("cute_craft/" + name + ".png")
 	stage.add_child(block)
 	block.x = x
 	block.y = y
 	block.alpha = a
-
+	block.rotation = r
 
 def spuzzle1():
-	for y in range(SQUARE):
+	square = 7
+	for y in range(square):
 		offset = y*WIDTH + WIDTH
-		add_block("grass1", y*WIDTH, y*HEIGHT, 1)
-		for x in range(SQUARE-y):
-			add_block("stone1", x*WIDTH + offset, y*HEIGHT, 1)
+		add_block("grass1", y*WIDTH, y*HEIGHT, 1, 0)
+		for x in range(square-y):
+			add_block("stone1", x*WIDTH + offset, y*HEIGHT, 1, 0)
 		for x in range(y):
-			add_block("sand1", x*WIDTH, y*HEIGHT, 1)
+			add_block("sand1", x*WIDTH, y*HEIGHT, 1, 0)
 
 def spuzzle2():
 	for y in range(4):
-		for x in range(8):
-			add_block("grass1", x*WIDTH, y*2*HEIGHT, math.fabs(-4+x)/4)
-			print math.fabs(-4+x)/4
+		for x in range(9):
+			add_block("grass1", x*WIDTH, y*2*HEIGHT, math.fabs(-4+x)/4, 0)
+
+def spuzzle3():
+	for y in range(8):
+		for x in range(9):
+			add_block("stone1", x*WIDTH, y*HEIGHT, y/7.0, (x/8.0)*math.pi)	
 
 spuzzle2()
 stage.run()

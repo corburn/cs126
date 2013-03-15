aef25ff8a0629b4a5d3e9c313d595585d77a5364
Jason Travis <infomaniac7@gmail.copm>
2013-03-14 20:52:40 -0700

    filter word list

--- lab7.py
+++ lab7.py
@@ -2,7 +2,16 @@ import random
 
 word_file = open("words.txt")
 words = word_file.readlines()
-word = random.choice(words).strip()
+print len(words)
+
+# Filter words that are not between 4 and 7 characters in length
+words = [word.strip() for word in words if len(word.strip()) >= 4 and len(word.strip()) <= 7]
+random_word = random.choice(words)
+# Filter words that are not the length of the random word
+words = [word for word in words if len(word) == len(random_word)]
+
+# Create a list of words that have no duplicate letters
+unique = ["".join(set(word)) for word in words]
 
 correct_guesses = []
 incorrect_guesses = []
@@ -16,7 +25,7 @@ def print_word():
 			s += "_ "
 	print s
 
-while len(incorrect_guesses) < 6:
+"""while len(incorrect_guesses) < 6:
 	print_word()
 	print "Please guess a letter."
 	guess = raw_input()
@@ -35,4 +44,48 @@ while len(incorrect_guesses) < 6:
 		print "Incorrect! You have " + \
 				str(6 - len(incorrect_guesses)) + \
 				" guesses left."
-print "The word was " + word + "."
+print "The word was " + word + "."""
+
+#for word in unique:
+	#print word
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+for l in alphabet:
+	count = 0
+	for word in unique:
+		if l in word:
+			count += 1
+	print l + ": " + str(count)
+index = -1
+size = 0
+for i, word in enumerate(unique):
+	if size < len(word):
+		size = len(word)
+		index = i
+print size
+print words[index]
+
+count = 0
+for word in unique:
+	if 'a' in word:
+		count += 1
+
+print count
+
+print "====="
+for i in range(len(random_word)-1):
+	count = 0
+	for word in words:
+		if 'a' in word[i]:
+			count += 1
+	print count
+print "+++++++"
+i_words = [i for i, word in enumerate(unique) if 'a' in word]
+print len(i_words)
+first = [i for i in i_words if words[i][0] == 'a']
+print len(first)
+
+def filter(letter, position):
+	return [word for word in words if letter not in word[position]]
+			

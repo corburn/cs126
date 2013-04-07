902fb4ca594282189034094df286f02c3d1e7256
Jason Travis <infomaniac7@gmail.copm>
2013-04-07 14:01:08 -0700

    update: convolute with objects to practice OOP

--- lab9.py
+++ lab9.py
@@ -13,32 +13,60 @@
 # Your game should correctly detect if either the player or the dealer busts to
 # determine the winner.
 
-#from __future__ import print_function
 import random
-import sys
+
+class Player:
+	cards = []
+	
+	def __init__(self):
+		cards = []
+		
+	def hit(self, card):
+		self.cards.append(card)
+		
+	def stand(self):
+		pass
+		
+	def getCards(self):
+		return self.cards
+
+class Dealer:
+	cards = []
+	
+	def __init__(self):
+		cards = []
+			
+	def hit(self, card):
+		self.cards.append(card)
+		
+	def getCards(self):
+		return self.cards
 
 class BlackJack:
 	deck = []
-	dealer = []
-	player = []
-
+	
+	# Add players
+	dealer = Dealer()
+	player = Player()
+	
 	def __init__(self):
 		# Create and shuffle the deck
 		for s in ["Hearts", "Diamonds", "Clubs", "Spades"]:
-			for n in range(1,13):
+			for n in range(1,14):
 				self.deck.append([n,s])
 		random.shuffle(self.deck)
+		
+		for i in range(2):
+			self.player.hit(self.deck.pop())
+		
+		while(self.getScore(self.dealer.getCards()) < 17):
+			self.dealer.hit(self.deck.pop())
 
-		# Deal two cards to each player
-		for n in range(2):
-			self.player.append(self.deck.pop())
-			self.dealer.append(self.deck.pop())
-
-	def getScore(self, player):
+	def getScore(self, cards):
 		score = 0
 		aces = 0
 		# soft score
-		for card in player:
+		for card in cards:
 			# count aces
 			if card[0] == 1:
 				score += 11
@@ -54,31 +82,31 @@ class BlackJack:
 				score -= 10
 		return score
 
-	def hit(self):
-		self.player.append(self.deck.pop())
-		return self.getScore(self.player)
-
 	def play(self):
 		move = ""
-		while move != "s" and self.getScore(self.player) <= 21:
-			sys.stdout.write("Dealer: ")
-			self.printHand(self.dealer)
-			sys.stdout.write("Player: ")
-			self.printHand(self.player)
+		while move != "s" and self.getScore(self.player.getCards()) <= 21:
+			print "Dealer: "
+			self.printHand(self.dealer.getCards()[1:2])
+			print "Player: "
+			self.printHand(self.player.getCards())
 			print "(h)it or (s)tand?"
 			move = raw_input()
 			if move == "h":
-				blackjack.hit()
+				self.player.hit(self.deck.pop())
 			if move == "s":
-				blackjack.stand()
+				self.player.stand()
 
-		dscore = self.getScore(self.dealer)
-		pscore = self.getScore(self.player)
+		print "Dealer: "
+		self.printHand(self.dealer.getCards())
+		print "Player: "
+		self.printHand(self.player.getCards())
+		dscore = self.getScore(self.dealer.getCards())
+		pscore = self.getScore(self.player.getCards())
 		# Push if both hands equal or both hands bust
 		if dscore == pscore or (dscore > 21 and pscore > 21):
 			print "Push!"
 		# Lose if player busts or scores less than dealer
-		elif pscore > 21 or dscore > pscore:
+		elif pscore > 21 or (dscore > pscore and dscore <= 21):
 			print "You lose!"
 		else:
 			print "You win!"
@@ -88,23 +116,16 @@ class BlackJack:
 		for card in hand:
 			if card[0] == 1:
 				s += "[Ace of " + card[1] + "]"
-			elif card[0] == 10:
-				s += "[Jack of " + card[1] + "]"
 			elif card[0] == 11:
-				s += "[Queen of " + card[1] + "]"
+				s += "[Jack of " + card[1] + "]"
 			elif card[0] == 12:
+				s += "[Queen of " + card[1] + "]"
+			elif card[0] == 13:
 				s += "[King of " + card[1] + "]"
 			else:
 				s += "[" + str(card[0]) + " of " + card[1] + "]"
 			s += " "
-		print s
-
-	def stand(self):
-		# Dealer draws until their score is greater than or equal to 17
-		while self.getScore(self.dealer) < 17:
-			self.dealer.append(self.deck.pop())
-		sys.stdout.write("Dealer: ")
-		self.printHand(self.dealer)
+		print s + str(self.getScore(hand))
 
 blackjack = BlackJack()
 blackjack.play()
